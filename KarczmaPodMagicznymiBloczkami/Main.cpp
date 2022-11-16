388d89f3200b4d7db7864b483d25dc3137150a29
Michał Kordos <michal.kordos8@gmail.com>
2022-11-16 14:55:36 +0100

    fix

--- KarczmaPodMagicznymiBloczkami/Main.cpp
+++ KarczmaPodMagicznymiBloczkami/Main.cpp
@@ -5,5 +5,5 @@ int main()
 {
     cout << "Testowanie tworzenia branchy i pull requestów" << endl;
     cout << "Dodatkowo merga sprawdze" << endl;
-    cout << "(polecam siê zapoznaæ z ww nazewnictwem bo w œwiecie programowania ca³y czas siê tego u¿ywa ;))"
+    cout << "(polecam siê zapoznaæ z ww nazewnictwem bo w œwiecie programowania ca³y czas siê tego u¿ywa ;))" << endl;
 }
