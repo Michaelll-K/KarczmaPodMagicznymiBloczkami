0a11b873fe10ba425818ffd3c9513bd33a190758
Grzegorz <grzegorz.firek044@gmail.com>
2022-11-30 12:10:29 +0100

    Do zweryfikowania wstępnego. Bałagan i wiem, że nie w mainie to powinno być, ale to ogarnę pod koniec

--- KarczmaPodMagicznymiBloczkami/KarczmaPodMagicznymiBloczkami.cpp
+++ KarczmaPodMagicznymiBloczkami/KarczmaPodMagicznymiBloczkami.cpp
@@ -2,16 +2,41 @@
 #include <fstream>
 #include <string>
 #include <cstdio>
+#include <cstdlib>
 #include "MenuManager.h"
+
 
 using namespace std;
 
 //Globalna zmienna uzupe³niana przez funkcjê GetMenu()
 extern string Menu[11][5];
 
+void ShowMenu() {
+    GetMenu("TestMenu.txt");
+    cout << "Nasze aktualne menu: \n" << endl;
+    for (int i = 0; i < 11; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << Menu[i][j] << " ";
+        }
+        cout  << endl;
+    }
+    cout << "\n" << endl;
+}
+
+
 int main()
 {
-    cout << "Testowanie tworzenia branchy i pull requestow" << endl;
-    cout << "Dodatkowo merga sprawdze" << endl;
-    cout << "(polecam sie zapoznac z ww nazewnictwem bo w swiecie programowania caly czas siê tego uzywa ;))" << endl;
+    ShowMenu();
+    int OrderSize;
+    cout << "Ile lacznie pozycji chcialbys zamowic? Wybierz, wpisujac cyfre z zakresu 1-11 " << endl;
+    cin >> OrderSize;
+    if (OrderSize < 1 || OrderSize > 11) {
+        cout << "\n Maksymalna ilosc pozycji w naszym menu, wynosi 11! Wybierz wartosc pomiedzy 1 a 11!\n";
+    }
+    else {
+        cout << "\n git";
+        //odt¹d lecê dalej z kodem
+    }
+
+    return 0;
 }
