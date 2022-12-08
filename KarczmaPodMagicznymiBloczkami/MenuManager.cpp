47fbab1aa932a678416edd26322ce9f67c5d54b4
Grzegorz <grzegorz.firek044@gmail.com>
2022-12-08 16:04:39 +0100

    Poprawa kodu związanego z edycją zamówienia w podsumowaniu

--- KarczmaPodMagicznymiBloczkami/MenuManager.cpp
+++ KarczmaPodMagicznymiBloczkami/MenuManager.cpp
@@ -113,39 +113,79 @@ void ShowDinnerAmountSummary()
 
 void DinnerSummaryEdit()
 {
-    int dinnerIndex;
-    int dinnerPortionIndex;
-    cout << "\n\nKtóre danie chcesz edytowaæ? Podaj numer pozycji \n";
-    cin >> dinnerIndex;
-    if (dinnerIndex < 1 || dinnerIndex > 11)
-    {
-        do
+    do {
+        int dinnerIndex;
+        int dinnerPortionIndex;
+        cout << "Które danie chcesz edytowaæ? Podaj numer pozycji \n";
+        cin >> dinnerIndex;
+        if (dinnerIndex < 1 || dinnerIndex > 11)
         {
-            cout << "Niepoprawna pozycja! WprowadŸ numer pozycji ponownie! (1 - 11)\n";
-            cin >> dinnerIndex;
-        } while (dinnerIndex < 1 || dinnerIndex>11);
-    }
+            do
+            {
+                cout << "Niepoprawna pozycja! WprowadŸ numer pozycji ponownie! (1 - 11)\n";
+                cin >> dinnerIndex;
+            } while (dinnerIndex < 1 || dinnerIndex>11);
+        }
 
 
-    cout << "Podaj now¹ iloœæ porcji \n";
-    cin >> dinnerPortionIndex;
+        cout << "Podaj now¹ iloœæ porcji \n";
+        cin >> dinnerPortionIndex;
 
-    if (dinnerPortionIndex < 0 || dinnerPortionIndex > 5)
-    {
-        do
+        if (dinnerPortionIndex < 0 || dinnerPortionIndex > 5)
         {
-            cout << "Niepoprawna iloœæ porcji! WprowadŸ numer porcji ponownie (1 - 5), lub wpisz 0, ¿eby zrezygnowaæ z tej pozycji! \n";
-            cin >> dinnerPortionIndex;
-        } while (dinnerPortionIndex < 0 || dinnerPortionIndex>5);
-    }
+            do
+            {
+                cout << "Niepoprawna iloœæ porcji! WprowadŸ numer porcji ponownie (1 - 5), lub wpisz 0, ¿eby zrezygnowaæ z tej pozycji! \n";
+                cin >> dinnerPortionIndex;
+            } while (dinnerPortionIndex < 0 || dinnerPortionIndex>5);
+        }
+
+
+        SummaryDinnerTab[dinnerIndex - 1] = dinnerPortionIndex;
+        cout << "Twoje zamówienie teraz to: \n";
+        cout << "Podsumowanie \n\n";
+        cout << "Twoje wybory, to: \n\n";
+        for (int k = 0; k < 11; k++)
+        {
+            if (SummaryDinnerTab[k] != 0)
+            {
+                cout << Menu[k][1] << "\n iloœæ porcji: " << SummaryDinnerTab[k] << "\n Pozycja w menu: " << Menu[k][0];
+                cout << endl;
+                cout << "\n" << endl;
+            }
+        }
+        do {
+            cout << "Czy potwierdzasz swoje zamówienie? \n";
+            cout << "1 - tak \n";
+            cout << "2 - nie \n";
+            cin >> confirm;
+        } while (confirm != 1 && confirm != 2);
+
+        if (confirm == 2)
+        {
+            DinnerSummaryEdit();
+        }
+        else
+        {
+            cout << "Dobry wybór! \n\n\n";
+        }
+
+        if (confirm != 1 && confirm != 2)
+        {
+            do
+            {
+                cout << "Wprowadzono inn¹ liczbê ni¿ 1 lub 2. Czy potwierdzasz swoje zamówienie? 1 = tak, 2 = nie \n";
+                cin >> confirm;
+            } while (confirm == 1 || confirm == 2);
+        }
+    } while (confirm == 2);
     cout << "Dobry wybór! \n";
 }
 
-
 void SummaryDinnerNameShow()
 {
-    cout << "\n\n\nPodsumowanie \n\n";
-    cout << "Twoje wybory, to: \n\n\n\n";
+    cout << "Podsumowanie \n\n";
+    cout << "Twoje wybory, to: \n\n";
     for (int k = 0; k < 11; k++)
     {
         if (SummaryDinnerTab[k] != 0)
@@ -165,13 +205,13 @@ void SummaryDinnerNameShow()
     if (confirm == 2)
     {
         DinnerSummaryEdit();
-        SummaryDinnerNameShow();
     }
     else
     {
-        cout << "\n\n\n\nDobry wybór! \n\n\n\n\n\n\n";
+        cout << "Dobry wybór! \n\n\n";
     }
 }
+
 
 
 void ShowFullVersionMenu() {
